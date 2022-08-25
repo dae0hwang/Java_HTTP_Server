fa2efcb1e1feb35b70f2343da63866cb0c765ee0
DaeYoung <geungan9@gmail.com>
2022-08-25 21:58:19 +0900

    feat: Add ReentrantLock in StringStorageClass

--- SimpleHTTP/src/main/java/StringStorage.java
+++ SimpleHTTP/src/main/java/StringStorage.java
@@ -1,26 +1,41 @@
-import java.io.DataOutputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class StringStorage {
     private static HashMap<String, String> stringStorage = new HashMap<>();
+    private static ReentrantLock lockForStringStorage = new ReentrantLock();
 
     public static void storeString(String textId, String str) {
-        stringStorage.put(textId, str);
+        lockForStringStorage.lock();
+        try {
+            stringStorage.put(textId, str);
+        } finally {
+            lockForStringStorage.unlock();
+        }
     }
 
     public static String getString(String textId) {
-        if (stringStorage.containsKey(textId)) {
-            String getString = stringStorage.get(textId);
-            return getString;
-        } else {
-            return null;
+        lockForStringStorage.lock();
+        try {
+            if (stringStorage.containsKey(textId)) {
+                String getString = stringStorage.get(textId);
+                return getString;
+            } else {
+                return null;
+            }
+        }finally {
+            lockForStringStorage.unlock();
         }
     }
 
     public  static void removeString(String textId) {
-        stringStorage.remove(textId);
+        lockForStringStorage.lock();
+        try {
+            stringStorage.remove(textId);
+        }finally {
+            lockForStringStorage.unlock();
+        }
     }
 
     public static Map<String, String> getMap() {
